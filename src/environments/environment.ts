7e579943771c887eed4dd108c6c200637496dce2
aschulte <aschulte@newfiregroup.com>
2019-03-15 09:08:24 -0500

    feat(app): complete app push

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD3tZ70k6zYLXikiwFoKuApEbJL8gLAVfQ",
+    authDomain: "create-tesla-rides.firebaseapp.com",
+    databaseURL: "https://create-tesla-rides.firebaseio.com",
+    projectId: "create-tesla-rides",
+    storageBucket: "create-tesla-rides.appspot.com",
+    messagingSenderId: "506990154547"
+  }
+};
